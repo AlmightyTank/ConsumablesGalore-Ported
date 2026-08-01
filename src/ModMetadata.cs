15a3633094cd677af0a150e4bf371a858a53fa48
Hamza <mhamzaiqbal1998@gmail.com>
2026-08-01 17:52:20 +0500

    update version to 2.0

--- src/ModMetadata.cs
+++ src/ModMetadata.cs
@@ -12,7 +12,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "MusicManiac-Consumables-Galore";
     public override string Author { get; init; } = "MusicManiac";
     public override List<string>? Contributors { get; init; } = ["AlmightyTank"];
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
